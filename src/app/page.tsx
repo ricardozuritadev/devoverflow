3bdba2e875cc8dca2d44e559710c7189d04c11e1
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-01-11 22:40:01 -0500

    Tailwind CSS fonts setup.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
     return (
-        <h1 className="text-3xl font-black">Welcome to the world of Next.js</h1>
+        <>
+            <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+
+            <h1 className="h1-bold font-space-grotesk">
+                Welcome to the world of Next.js
+            </h1>
+        </>
     );
 }
