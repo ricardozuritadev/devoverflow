58af3bc5ce9bd4554e23671ca661f389dc76a17a
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-01-11 21:42:01 -0500

    ESLint config update.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+        "plugin:tailwindcss/recommended",
+        "prettier"
+    ),
+    {
+        ignores: ["node_modules/", "dist/", "build/", "*.test.js"]
+    }
 ];
 
 export default eslintConfig;
