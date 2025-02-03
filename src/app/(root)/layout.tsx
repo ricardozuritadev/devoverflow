4c29402785c66b458ba1cf7a20dd299fa90de39e
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-02-03 13:04:45 -0500

    Create LeftSidebar component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 
 import NavBar from "@/components/navigation/navbar/NavBar";
+import LeftSidebar from "@/components/navigation/left-sidebar/LeftSidebar";
 
-export default function RootLayout({children}: {children: ReactNode}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <main>
             <NavBar />
+            <LeftSidebar />
             {children}
         </main>
     );
---
+++ src/components/navigation/left-sidebar/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+import NavLinks from "@/components/navigation/navbar/NavLinks";
+
+export default function LeftSidebar() {
+    return (
+        <nav className="">
+            <NavLinks />
+        </nav>
+    );
+}
