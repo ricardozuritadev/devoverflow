399121314cf614b4d94ab2c51dbbe8134534988a
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-02-04 07:41:54 -0500

    Build desktop left Sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -5,10 +5,16 @@ import LeftSidebar from "@/components/navigation/left-sidebar/LeftSidebar";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <main>
+        <main className="background-light850_dark100 relative">
             <NavBar />
-            <LeftSidebar />
-            {children}
+
+            <div className="flex">
+                <LeftSidebar />
+
+                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+                    <div className="mx-auto w-full max-w-5xl">{children}</div>
+                </section>
+            </div>
         </main>
     );
 }
--- src/components/navigation/left-sidebar/LeftSidebar.tsx
+++ src/components/navigation/left-sidebar/LeftSidebar.tsx
@@ -1,9 +1,51 @@
+import Link from "next/link";
+import ROUTES from "@/constants/route";
+
+import { Button } from "@/components/ui/button";
 import NavLinks from "@/components/navigation/navbar/NavLinks";
+import Image from "next/image";
 
 export default function LeftSidebar() {
     return (
-        <nav className="">
-            <NavLinks />
-        </nav>
+        <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+            <div className="flex flex-1 flex-col gap-6">
+                <NavLinks />
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <Button
+                    className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+                    asChild
+                >
+                    <Link href={ROUTES.SIGN_IN}>
+                        <Image
+                            src="/icons/account.svg"
+                            alt="Account"
+                            width={20}
+                            height={20}
+                            className="invert-colors lg:hidden"
+                        />
+
+                        <span className="primary-text-gradient max-lg:hidden">
+                            Log In
+                        </span>
+                    </Link>
+                </Button>
+
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                    <Link href={ROUTES.SIGN_IN}>
+                        <Image
+                            src="/icons/sign-up.svg"
+                            alt="Account"
+                            width={20}
+                            height={20}
+                            className="invert-colors lg:hidden"
+                        />
+
+                        <span className="max-lg:hidden">Sign Up</span>
+                    </Link>
+                </Button>
+            </div>
+        </section>
     );
 }
