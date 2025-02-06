4be50c2b2b4083dc894236dd1a115212ebd2c0ac
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-02-06 11:42:42 -0500

    Implement Home layout

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,28 +1,28 @@
-import { auth, signOut } from "@/auth";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import ROUTES from "@/constants/route";
 
-import { Button } from "@/components/ui/button";
-
 export default async function Home() {
-    const session = await auth();
-
-    console.log(session);
-
     return (
         <>
-            <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+            <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+                <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-            <form
-                className="px-10 pt-[100px]"
-                action={async () => {
-                    "use server";
-
-                    await signOut({
-                        redirectTo: ROUTES.SIGN_IN
-                    });
-                }}
-            ></form>
+                <Button
+                    className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+                    asChild
+                >
+                    <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+                </Button>
+            </section>
+            <section className="mt-11">LocalSearch</section>
+            HomeFilter
+            <div className="mt-10 flex w-full flex-col gap-6">
+                <p>Question card 1</p>
+                <p>Question card 2</p>
+                <p>Question card 3</p>
+                <p>Question card 4</p>
+            </div>
         </>
     );
 }
